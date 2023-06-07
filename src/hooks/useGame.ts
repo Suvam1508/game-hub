e988628c8d9460a5ef969ae6b79e7f8f0d506bec
Suvam Chatterjee <89910679+Suvam1508@users.noreply.github.com>
2023-06-07 12:03:52 +0530

    Filter Games by platforms

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -20,13 +20,19 @@ export interface Game {
 //   results: Game[];
 // }
 
-const useGames = (selectedGenre: Genre | null) =>
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
   useData<Game>(
     "/games",
     {
-      params: { genres: selectedGenre?.id },
+      params: {
+        genres: selectedGenre?.id,
+        platforms: selectedPlatform?.id,
+      },
     },
-    [selectedGenre?.id]
+    [selectedGenre?.id, selectedPlatform?.id]
   );
 
 export default useGames;
